000449f08a459b6e51467379b951d8f3f294d4c1
jsantos <jsantos@student.42lisboa.com>
2021-10-04 16:13:45 +0100

    Now also frees memory of paths var.

--- srcs/main.c
+++ srcs/main.c
@@ -1,22 +1,25 @@
 #include "../headers/main.h"
 
-char	**get_paths(char **env);
+char	**get_paths(char **env, int *nb_paths);
 
 int main(int argc, char **argv, char **env)
 {
 	char	**paths;
+	int		nb_paths;
 
 	(void)argc;
 	(void)argv;
-	paths = get_paths(env);
+	paths = get_paths(env, &nb_paths);
 //	int iter = -1;
 //	while (paths[++iter] != NULL)
 //		printf("%s\n", paths[iter]);
+	if (paths)
+		ft_free_matrix((void**)paths, nb_paths);
 	return 0;
 }
 
 
-char	**get_paths(char **env)
+char	**get_paths(char **env, int *nb_paths)
 {
 	char	*paths_str;
 	char	**paths;
@@ -32,7 +35,7 @@ char	**get_paths(char **env)
 			break ;
 		}
 	}
-	paths = ft_split(paths_str, ':');
+	paths = ft_split(paths_str, ':', nb_paths);
 	if (!paths)
 		handle_error(FAILED_MALLOC);
 	return (paths);
