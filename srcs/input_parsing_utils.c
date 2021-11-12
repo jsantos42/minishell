acf354aa01ac52334c45652a93ff2a8ed02cc6ac
jsantos <jsantos@student.42lisboa.com>
2021-11-12 12:49:50 +0000

    Refactoring the advance_to_closing_quote according to the one made on the other branch.
    This will prove useful to remove the quotes, but it could also be a model for the handle_dollar_char.

--- srcs/input_parsing_utils.c
+++ srcs/input_parsing_utils.c
@@ -48,3 +48,63 @@ int	advance_to_closing_quote(char *str)
 	terminate_program(UNCLOSED_QUOTES);
 	return (0);
 }
+
+
+
+if (scanner->cursor[iter] == '\"' || scanner->cursor[iter] == '\'')
+scanner->cursor = handle_quote(scanner->cursor, &iter);
+
+/*
+**	This makes sure nothing between quotes gets interpreted as a command.
+**	1) It starts by defining which type of quote it's using.
+**	2) Creates a new_input string, which will be a copy of input without the
+**	pair of quotes it's handling.
+**	3) Parses through the input string and copies it to new_input, except for
+**	the quote_type char (unless it's escaped). Continues to do so until it finds
+**	a closing quote.
+**	4) If it found a closing quote, saves the new position (that will be passed
+**	to scanner->cursor) and copies the rest of the input string to new_input,
+**	NULL-terminating it and returning it.
+**	NOTE: when saving the new quote_pos, it uses kter (instead of iter) because
+**	it needs to subtract 2 quote chars from the index count. Plus, it needs to
+**	subtract 1 because the iteration on the previous while goes one extra loop.
+*/
+
+char	*handle_quote(char *input, int *quote_pos)
+{
+	char	quote_type;
+	char	*new_input;
+	int 	quotes_found;
+	int		iter;
+	int		kter;
+
+	quote_type = *(input + *quote_pos);
+	new_input = malloc(ft_strlen(input) - PAIR_OF_QUOTES + 1);
+	if (!new_input)
+		terminate_program(MALLOC);
+	quotes_found = 0;
+	iter = 0;
+	kter = 0;
+	while (input[iter] != '\0' && quotes_found < 2)
+	{
+		if (!(input[iter] == quote_type
+			  && (iter == 0 || input[iter - 1] != '\\')))
+			new_input[kter++] = input[iter];
+		else
+			(quotes_found)++;
+		iter++;
+	}
+	if (quotes_found < 2)
+	{
+		free(new_input);
+		terminate_program(UNCLOSED_QUOTES);
+	}
+	else
+	{
+		*quote_pos += kter - 1;
+		while (input[iter] != '\0')
+			new_input[kter++] = input[iter++];
+		new_input[kter] = '\0';
+	}
+	return (new_input);
+}
