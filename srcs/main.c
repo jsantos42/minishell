f12b910154ce7b23de88494906f26c0d06fd9ff9
pbielinux <pedrobielik@hotmail.com>
2021-11-23 21:36:49 +0000

    Ninja turtles on

--- srcs/main.c
+++ srcs/main.c
@@ -1,18 +1,26 @@
 #include "../headers/main.h"
 
+static char *prompt[] = {
+	"\n\033[38;5;214m𓆉  Minishell $ ",
+	"\n\033[38;5;196m𓆉  Minishell $ ",
+	"\n\033[38;5;93m𓆉  Minishell $ ",
+	"\n\033[38;5;39m𓆉  Minishell $ "
+	};
 int main(int argc, char **argv, char **envp)
 {
 	t_data	data;
+	int		i = 0;
 
 	init_data(&data, argc, argv, envp);
 	while (!data.exit_cmd)
 	{
-		data.input = readline(PROMPT);
+		data.input = readline(prompt[i]);
 		if (parse_input(&data))
 			execute_input(&data);
 //		free_commands(data.commands);
 		free_if_not_null(data.input);
 		data.input = NULL;
+		i = i != 3 ? i + 1 : 0;
 	}
 	free_data();
 	return (0);
