ea9df5bb8249711c14c1491dc140611ecf52440f
pbielinux <pedrobielik@hotmail.com>
2021-12-06 16:25:04 +0000

    redirection working on simple commands

--- srcs/redirect_output.c
+++ srcs/redirect_output.c
@@ -20,6 +20,7 @@ void	handle_output_redirection(t_tree **current_node, char **str, int *i)
 			*str = handle_quote_char(*str, i);
 		else if (is_dollar_sign((*str)[*i]))
 			*str = handle_dollar_sign(*str, *i);
+		(*i)++;
 	}
 	if (*i != old_i)
 		(*current_node)->leaf.redir_output = ft_substr(*str, old_i, *i);
