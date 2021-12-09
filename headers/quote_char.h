d344396f4b22aa893e0214922c81796a173b0226
jsantos <jsantos@student.42lisboa.com>
2021-12-09 10:54:33 +0000

    Solved echo a"$PATH"b
    Not expanding a second variable such as echo "$PATH$HOME", working on
    it.

--- headers/quote_char.h
+++ headers/quote_char.h
@@ -7,6 +7,6 @@
 
 bool	is_quote_char(char chr);
 void	handle_quote_char(t_data *data, int *quote_pos);
-void	look_for_expansions(t_data *data, int i, int end);
+void	look_for_expansions(t_data *data, int i, int *end);
 
 #endif
--- srcs/quote_char.c
+++ srcs/quote_char.c
@@ -16,58 +16,56 @@ bool	is_quote_char(char chr)
 **	4) If it found a closing quote, saves the new position (that will be passed
 **	to scanner->cursor) and copies the rest of the input string to new_input,
 **	NULL-terminating it and returning it.
-**	NOTE: when saving the new quote_pos, it uses j (instead of i) because
+**	NOTE: when saving the new opening_quote, it uses j (instead of i) because
 **	it needs to subtract 2 quote chars from the index count. Plus, it needs to
 **	subtract 1 because the iteration on the previous while goes one extra loop.
 */
 
-void	handle_quote_char(t_data *data, int *quote_pos)
+void	handle_quote_char(t_data *data, int *opening_quote)
 {
 	char	quote_type;
 	char	*new_input;
-	bool	closing_quote;
+//	bool	closing_quote;
 	int		i;
 	int		j;
-	int		original_quote_pos;
+	int		closing_quote;
+//	int		original_quote_pos;
 
 	if (data->escaped)
 	{
-		(*quote_pos)++;
+		(*opening_quote)++;
 		data->escaped = false;
 		return ;
 	}
-	original_quote_pos = *quote_pos;
-	quote_type = *(data->input + *quote_pos);
-	new_input = malloc(ft_strlen(data->input) - 2 * QUOTE_CHAR + 1);
-	if (!new_input)
-		terminate_program(MALLOC);
-	closing_quote = false;
-	i = 0;
-	j = 0;
-	while (i != *quote_pos)
-		new_input[j++] = data->input[i++];
+//	original_quote_pos = *opening_quote;
+	quote_type = *(data->input + *opening_quote);
+	closing_quote = 0;
+	i = *opening_quote;
 	while (!closing_quote && data->input[++i] != '\0')
 	{
 		if (data->input[i] == quote_type && !is_escaped(data->input, i))
-			closing_quote = true;
-		else
-		{
-			new_input[j++] = data->input[i];
-			(*quote_pos)++;
-		}
+			closing_quote = i;
 	}
 	if (!closing_quote)
-	{
-		free(new_input);
 		terminate_program(UNCLOSED_QUOTES);
-	}
+	if (quote_type == '\"')
+		look_for_expansions(data, *opening_quote, &closing_quote);
+	new_input = malloc(ft_strlen(data->input) - 2 * QUOTE_CHAR + 1);
+	if (!new_input)
+		terminate_program(MALLOC);
+	i = -1;
+	j = -1;
 	while (data->input[++i] != '\0')
-		new_input[j++] = data->input[i];
-	new_input[j] = '\0';
+	{
+		if (i == *opening_quote || i == closing_quote)
+			continue ;
+		new_input[++j] = data->input[i];
+		if (i > *opening_quote)
+			(*opening_quote)++;
+	}
+	new_input[++j] = '\0';
 	free(data->input);
 	data->input = new_input;
-	if (quote_type == '\"')
-		look_for_expansions(data, original_quote_pos, *quote_pos);
 }
 
 
@@ -76,17 +74,17 @@ void	handle_quote_char(t_data *data, int *quote_pos)
  *	the sum.
 */
 
-void	look_for_expansions(t_data *data, int i, int end)
+void	look_for_expansions(t_data *data, int i, int *end)
 {
-	while (i < end)
+	while (i < *end)
 	{
 		if (is_escape_char(data->input[i])
 		&& (is_dollar_sign(data->input[i + 1]) || data->input[i + 1] == '\"'))
 			remove_escape_char(data, &i);
 		else if (is_dollar_sign(data->input[i]))
 		{
-			end -= get_var_length(data->input + i + DOLLAR_SIGN);
-			end += handle_dollar_sign(data, &i);
+			*end -= get_var_length(data->input + i + DOLLAR_SIGN) + DOLLAR_SIGN;
+			*end += handle_dollar_sign(data, &i);
 		}
 		else
 			data->escaped = false;
